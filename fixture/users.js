031815b64df38876029def27ed3b509fd5066a01
SriramKothapalli <skothapall@phizzle.com>
2024-01-16 20:43:58 +0530

    latest

--- fixture/users.js
+++ fixture/users.js
@@ -1,9 +1,9 @@
 export const userEquipmentMap = {
     "SRIRAM-OPERATOR": { equipment: "SE054003", simUI: "http://172.168.9.54:7103" },
-    // "AKANKSHA-OPERATOR": { equipment: "SE054004", simUI: "http://172.168.9.54:7104" },
-    // "phz-op10": { equipment: "SE054005", simUI: "http://172.168.9.54:7105" },
-    // "phz-op15": { equipment: "SE054006", simUI: "http://172.168.9.54:7106" },
-    // "phz-op12": { equipment: "SE054007", simUI: "http://172.168.9.54:7107" },
-    // "phz-op13": { equipment: "SE054008", simUI: "http://172.168.9.54:7108" },
-    // "phz-op14": { equipment: "SE054009", simUI: "http://172.168.9.54:7109" },
+    "AKANKSHA-OPERATOR": { equipment: "SE054004", simUI: "http://172.168.9.54:7104" },
+    "phz-op10": { equipment: "SE054005", simUI: "http://172.168.9.54:7105" },
+    "phz-op15": { equipment: "SE054006", simUI: "http://172.168.9.54:7106" },
+    "phz-op12": { equipment: "SE054007", simUI: "http://172.168.9.54:7107" },
+    "phz-op13": { equipment: "SE054008", simUI: "http://172.168.9.54:7108" },
+    "phz-op14": { equipment: "SE054009", simUI: "http://172.168.9.54:7109" },
 };
